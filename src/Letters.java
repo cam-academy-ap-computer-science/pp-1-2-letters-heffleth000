5f8fc93435ffabb398610f93f6f2298f2ddfdbc5
heffleth000 <heffleth000@CAM-LAB-17.bgsd.k12.wa.us>
2019-09-16 07:55:35 -0700

    Letters halfway done

--- src/Letters.java
+++ src/Letters.java
@@ -3,7 +3,50 @@ public class Letters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Parents();
+		Brother();
+		Friend();
+	}
+	public static void Parents() {
+		System.out.println("Dear Mom and Dad,");
+		System.out.println("");
+		Classes();
+		Friends();
+		Money();
+		From();
+	}
+	public static void Brother() {
+		System.out.println("Dear Evan,");
+		System.out.println("");
+		Hobbies();
+		Friends();
+		Money();
+		From();
+	}
+	public static void Friend() {
+		System.out.println("Dear Friend,");
+		System.out.println("");
+		Hobbies();
+		Classes();
+		Advice();
+		From();
+	}
+	public static void From() {
+		System.out.println("");
+		System.out.println("Love,");
+		System.out.println("Ethan Hefflin");
+	}
+	public static void Classes() {
+		System.out.println("\tMy classes this year are going great! I'm taking three AP classes this year");
+		System.out.println("so it will be hard, but so far they're not stressing me out too much. The one");
+		System.out.println("I was most worried about was AP Computer Science, but it turns out I'm really");
+		System.out.println("enjoying it so far. It's my favorite out of all of my classes and is the most");
+		System.out.println("interesting. I'm a little lost in Pre Calculus because I haven't taken \"real\"");
+		System.out.println("math in over a yearbecause of AP Statistics, but I'm getting by. Overall, just");
+		System.out.println("I'm excited to see how this school year goes!")
+	}
+	public static void Friends() {
+		System.out.println("\tBecause of senior class is so small and we've been together");
 	}
 
 }
