bc8ddebf777df32347a7b954f8e82ab238525625
Ethan <Ethan@172.20.10.2>
2019-09-16 09:53:42 -0700

    Letters done

--- src/Letters.java
+++ src/Letters.java
@@ -4,7 +4,9 @@ public class Letters {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Parents();
+		System.out.println("");
 		Brother();
+		System.out.println("");
 		Friend();
 	}
 	public static void Parents() {
@@ -37,16 +39,45 @@ public class Letters {
 		System.out.println("Ethan Hefflin");
 	}
 	public static void Classes() {
-		System.out.println("\tMy classes this year are going great! I'm taking three AP classes this year");
-		System.out.println("so it will be hard, but so far they're not stressing me out too much. The one");
-		System.out.println("I was most worried about was AP Computer Science, but it turns out I'm really");
-		System.out.println("enjoying it so far. It's my favorite out of all of my classes and is the most");
-		System.out.println("interesting. I'm a little lost in Pre Calculus because I haven't taken \"real\"");
-		System.out.println("math in over a yearbecause of AP Statistics, but I'm getting by. Overall, just");
-		System.out.println("I'm excited to see how this school year goes!")
+		System.out.println("\tMy classes this year are going great! I'm taking three AP classes this");
+		System.out.println("year so it will be hard, but so far they're not stressing me out too much. The");
+		System.out.println("one I was most worried about was AP Computer Science, but it turns out I'm");
+		System.out.println("really enjoying it so far. It's my favorite out of all of my classes and is the");
+		System.out.println("most interesting. I'm a little lost in Pre Calculus because I haven't taken \"real\"");
+		System.out.println("math in over a year because of AP Statistics, but I'm getting by. Overall, just");
+		System.out.println("I'm excited to see how this school year goes!");
 	}
 	public static void Friends() {
-		System.out.println("\tBecause of senior class is so small and we've been together");
+		System.out.println("\tBecause of senior class is so small and we've been together for over a");
+		System.out.println("year now, this year has been a lot more enjoyable. Most of our grade is");
+		System.out.println("friends with each other (a big improvement from these last few years). Hopefully");
+		System.out.println("our class keeps up with this and keeps interacting with each other. It would make");
+		System.out.println("the year a lot more fun!");
+	}
+	public static void Hobbies() {
+		System.out.println("\tRight now I'm playing tennis for Prairie Highschool. I'm playing doubles");
+		System.out.println("with my friend Cole. We're hoping to make it really far this year because it's");
+		System.out.println("our last, so hopefully we can play well and improve. I'll start training for");
+		System.out.println("baseball this fall and throughout the winter. This next season I hope I can be ");
+		System.out.println("the varsity shortstop, but it'll be tough. Other than tennis and baseball, I haven't");
+		System.out.println("been doing much other than schoolwork and volunteering. I've been volunteering with");
+		System.out.println("Clark PUD's Stream Team and it's been fun. We get to plant trees and pull");
+		System.out.println("invasive plants, but my favorite part is when we get to hike around streams to");
+		System.out.println("check for invasive plants or water quality. That part is a lot of fun.");
+	}
+	public static void Money() {
+		System.out.println("\tSo I know you don't want to hear this, but I need money now. I spent it");
+		System.out.println("all buying tables to I can stack them 100 feet high. The plan hasn't worked");
+		System.out.println("because I ran out of tables at around 57 feet. I really need to finish the last");
+		System.out.println("43 feet, so any money you can give to this wonderful cause would be graciously.");
+		System.out.println("I need about $320,000 more.");
+	}
+	public static void Advice() {
+		System.out.println("\tSo obviously you know about my table stacking idea, but I'm trying to");
+		System.out.println("decide if I need to change directions. I don't know if people would admire my");
+		System.out.println("table tower as much as they might like a tower made of recycling bins. Would");
+		System.out.println("you happen to know anyone that could help me sell off my tables and buy");
+		System.out.println("recycling bins? Do you have any suggestions on what I should do?");
 	}
 
 }
